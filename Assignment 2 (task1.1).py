f57235c869cba7095a00abcfa5c8948f57c4746b
Simar bhatia <simar74bhatia@gmail.com>
2025-04-23 21:52:43 +0530

    Add files via upload

---
+++ Assignment 2 (task1.1).py
@@ -0,0 +1,18 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/Pc/AppData/Local/Programs/Python/Python313/Assignment 2 (task1).py
+Enter a number: 10
+Even number
+>>> 
+= RESTART: C:/Users/Pc/AppData/Local/Programs/Python/Python313/Assignment 2 (task1).py
+Enter a number: 4
+Even number
+>>> 
+= RESTART: C:/Users/Pc/AppData/Local/Programs/Python/Python313/Assignment 2 (task1).py
+Enter a number: 3
+Odd number
+>>> 
+= RESTART: C:/Users/Pc/AppData/Local/Programs/Python/Python313/Assignment 2 (task1).py
+Enter a number: 1
+Odd number
